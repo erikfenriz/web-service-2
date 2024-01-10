dc6aa690829300aa72d0ee5207e30ada9325b2b1
erikfenriz <erikfenriz@gmail.com>
2024-01-10 21:02:13 +0100

    awfawfad

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const db = require('./db');
 const dotenv = require('dotenv');
+const express = require('express');
 
 dotenv.config();
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+})
+
+app.listen(process.env.PORT || 3000);
+console.log()
+
 db.connect().catch(console.error);
+
